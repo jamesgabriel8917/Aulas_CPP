79561a8730d22b7165d64a93fdc3b12dd3011d56
James Gabriel Bes Fontana <jamesgabriel8917@gmail.com>
2024-07-05 20:46:53 -0300

    relembrando a sintaxe

---
+++ jogoDaAdivinhacao/jogodaadivinhacao.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+
+int main (){
+    // Configurar a localidade para UTF-8
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+
+
+    cout<<"*************************************\n";
+    cout<<"* Bem vindos ao jogo da adivinhação *\n";
+    cout<<"*************************************\n";
+
+    int numeroSecreto = 42;
+
+    
+
+}
