f4a33f40d7d9a06d7e0b2080512824237969caf7
James Gabriel Bes Fontana <jamesgabriel8917@gmail.com>
2024-07-06 23:03:50 -0300

    criando jogo da forca

--- jogoDaForca/jogoDaForca.cpp
+++ jogoDaForca/jogoDaForca.cpp
@@ -1,11 +1,49 @@
 #include <iostream>
+#include <string>
+
+
 using namespace std;
+
+bool chute_existe(char chute, string PALAVRA_SECRETA){
+
+    for(char letra : PALAVRA_SECRETA){
+        if(chute == letra){
+            return true;
+        }
+    }
+    return false;
+}
 
 
 int main(){
+    system("cls");
 
-    string palavra_secreta = "PUTADA"; 
+    const string PALAVRA_SECRETA = "PUTADA"; 
+
+    bool nao_acertou = true;
+    bool nao_enforcou = true;
+
+
+
+    while (nao_acertou && nao_enforcou){
+
+        char chute;
+        cout<<"Digite o seu chute\n";
+        cin>>chute;
+
+        if (chute_existe(chute, PALAVRA_SECRETA))
+        {
+            cout<<"PARABENS!! Seu chute esta contido na palavra\n";
+        }else{
+            cout<<"Errou!\n";
+        }
+        
+        
+    }
     
 
 
+
 }
+
+
