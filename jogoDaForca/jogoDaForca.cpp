c3b831d3df2c7d63dea72ee97121efa522881640
James Gabriel Bes Fontana <jamesgabriel8917@gmail.com>
2024-07-07 21:11:10 -0300

    update

--- jogoDaForca/jogoDaForca.cpp
+++ jogoDaForca/jogoDaForca.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <vector>
 
 
 using namespace std;
+
+map<char, bool>chutou;
+vector<char> chutes_errados;
 
 bool chute_existe(char chute, string PALAVRA_SECRETA){
 
@@ -14,36 +19,68 @@ bool chute_existe(char chute, string PALAVRA_SECRETA){
     return false;
 }
 
+bool nao_acertou(string PALAVRA_SECRETA){
+    for(char letra : PALAVRA_SECRETA){
+        if(!chutou[letra]){
+            return true;
+        }
+    }
+    return false;
+}
+
+bool nao_enforcou(){
+    return chutes_errados.size() < 5;
+}
+
 
 int main(){
     system("cls");
 
+    cout<<"**************************************\n";
+    cout<<"*           JOGO DA FORCA            *\n";
+    cout<<"**************************************\n";
+
+
     const string PALAVRA_SECRETA = "PUTADA"; 
+    
 
-    bool nao_acertou = true;
-    bool nao_enforcou = true;
+    while (nao_acertou(PALAVRA_SECRETA) && nao_enforcou()){
+        system("cls");
+        cout<<"Chutes errados: ";
+        for (char letra : chutes_errados)
+        {
+            cout<<letra<<" ";
+        }
+        cout<<"\n";
 
+        for(char letra : PALAVRA_SECRETA){
+            if(chutou[letra])
+            {
+                cout<< letra <<" ";
+            }else{
+                cout<< "_ ";
+            }
 
-
-    while (nao_acertou && nao_enforcou){
+            
+        }
 
         char chute;
-        cout<<"Digite o seu chute\n";
+        cout<<"\nDigite o seu chute:     ";
         cin>>chute;
+
+        chutou[chute] = true;
 
         if (chute_existe(chute, PALAVRA_SECRETA))
         {
             cout<<"PARABENS!! Seu chute esta contido na palavra\n";
         }else{
             cout<<"Errou!\n";
+            chutes_errados.push_back(chute);
         }
         
         
     }
     
-
-
-
 }
 
 
