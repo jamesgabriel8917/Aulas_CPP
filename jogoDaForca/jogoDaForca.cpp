0fce8a3a77d0fbbbf54bf5c95adb008ac214f172
James Gabriel Bes Fontana <jamesgabriel8917@gmail.com>
2024-07-07 21:37:17 -0300

    update

--- jogoDaForca/jogoDaForca.cpp
+++ jogoDaForca/jogoDaForca.cpp
@@ -9,6 +9,34 @@ using namespace std;
 map<char, bool>chutou;
 vector<char> chutes_errados;
 
+void imprime_cabecalho(){
+    system("cls");
+
+    cout<<"**************************************\n";
+    cout<<"*           JOGO DA FORCA            *\n";
+    cout<<"**************************************\n";
+}
+
+void imprime_erros(){
+    cout<<"Chutes errados: ";
+    for (char letra : chutes_errados)
+    {
+        cout<<letra<<" ";
+    }
+    cout<<"\n";
+}
+
+void imprime_palavra(string PALAVRA_SECRETA){
+    for(char letra : PALAVRA_SECRETA){
+    if(chutou[letra])
+    {
+        cout<< letra <<" ";
+    }else{
+        cout<< "_ ";
+    }
+}
+}
+
 bool chute_existe(char chute, string PALAVRA_SECRETA){
 
     for(char letra : PALAVRA_SECRETA){
@@ -32,38 +60,7 @@ bool nao_enforcou(){
     return chutes_errados.size() < 5;
 }
 
-
-int main(){
-    system("cls");
-
-    cout<<"**************************************\n";
-    cout<<"*           JOGO DA FORCA            *\n";
-    cout<<"**************************************\n";
-
-
-    const string PALAVRA_SECRETA = "PUTADA"; 
-    
-
-    while (nao_acertou(PALAVRA_SECRETA) && nao_enforcou()){
-        system("cls");
-        cout<<"Chutes errados: ";
-        for (char letra : chutes_errados)
-        {
-            cout<<letra<<" ";
-        }
-        cout<<"\n";
-
-        for(char letra : PALAVRA_SECRETA){
-            if(chutou[letra])
-            {
-                cout<< letra <<" ";
-            }else{
-                cout<< "_ ";
-            }
-
-            
-        }
-
+void chuta(string PALAVRA_SECRETA){
         char chute;
         cout<<"\nDigite o seu chute:     ";
         cin>>chute;
@@ -77,10 +74,35 @@ int main(){
             cout<<"Errou!\n";
             chutes_errados.push_back(chute);
         }
-        
+}
+
+
+int main(){
+
+    imprime_cabecalho();
+
+
+    const string PALAVRA_SECRETA = "MORANGO"; 
+    
+
+    while (nao_acertou(PALAVRA_SECRETA) && nao_enforcou()){
+
+        imprime_erros();
+        imprime_palavra(PALAVRA_SECRETA);
+        chuta(PALAVRA_SECRETA);
         
     }
+
+    system("cls");
     
+    cout<<"Fim de jogo!\n";
+    if(nao_acertou(PALAVRA_SECRETA)){
+        cout<<"Tente novamente\n";
+    }else{
+        cout<<"Parabens, vc acertou a palavra secreta!!\n";
+        
+    }
+    cout<<"A palavra secreta e: "<<PALAVRA_SECRETA<<"\n";
 }
 
 
